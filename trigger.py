5951975a2af59b0cd1e7e0a66e07845a2a4a262e
KanishkRajMittal <152096720+KanishkRajMittal@users.noreply.github.com>
2023-11-29 00:29:33 +0530

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 
 def run_my_script():
-    subprocess.run(["python3", "/home/kanishk/Desktop/Python/firebase/profile_maker.py"])
+    subprocess.run(["python3", "potd.py.py"])
 
 # Schedule the script to run every 2 minutes
 schedule.every(2).minutes.do(run_my_script)
