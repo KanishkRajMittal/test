41c46a0b2d513f13c998871af96a9412dc115408
KanishkRajMittal <152096720+KanishkRajMittal@users.noreply.github.com>
2023-11-28 21:32:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+# app.py
+from flask import Flask, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/potd>', methods=['POST','GET'])
+def POTD(gmail):
+    try:
+        # Run your Python script using subprocess
+        result = subprocess.run(['python3', 'potd.py.py'], capture_output=True, text=True)
+        output = result.stdout
+        return jsonify({'success': True, 'output': output})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#https://profile-mfru.onrender.com/update
